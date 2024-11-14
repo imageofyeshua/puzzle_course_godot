1c0e06ee1a8d952baa2677809d08c45ba5f10436
Daniel Park <intheimageofyeshua@gmail.com>
2024-11-14 12:41:23 +0900

    TileMapLayer Added

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -8,6 +8,8 @@ public partial class Main : Node2D
 	private Sprite2D cursor;
 	private PackedScene buildingScene;
 	private Button placeBuildingButton;
+	private TileMapLayer hightlightTileMapLayer;
+	private Vector2? hoveredGridCell;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -15,6 +17,7 @@ public partial class Main : Node2D
 		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
 		cursor = GetNode<Sprite2D>("Cursor");
 		placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
+		hightlightTileMapLayer = GetNode<TileMapLayer>("HighlightTileMapLayer");
 
 		cursor.Visible = false;
 
@@ -36,6 +39,12 @@ public partial class Main : Node2D
 	{
 		var gridPosition = GetMouseGridCellPosition();
 		cursor.GlobalPosition = gridPosition * 64;
+
+		if (cursor.Visible && (!hoveredGridCell.HasValue || hoveredGridCell.Value != gridPosition))
+		{
+			hoveredGridCell = gridPosition;
+			UpdateHighlightTileMapLayer();
+		}
 	}
 
 	private Vector2 GetMouseGridCellPosition()
@@ -53,6 +62,27 @@ public partial class Main : Node2D
 
 		var gridPosition = GetMouseGridCellPosition();
 		building.GlobalPosition = gridPosition * 64;
+
+		hoveredGridCell = null;
+		UpdateHighlightTileMapLayer();
+	}
+
+	private void UpdateHighlightTileMapLayer()
+	{
+		hightlightTileMapLayer.Clear();
+
+		if (!hoveredGridCell.HasValue)
+		{
+			return;
+		}
+
+		for (var x = hoveredGridCell.Value.X - 3; x <= hoveredGridCell.Value.X + 3; x++)
+		{
+			for (var y = hoveredGridCell.Value.Y - 3; y <= hoveredGridCell.Value.Y + 3; y++)
+			{
+				hightlightTileMapLayer.SetCell(new Vector2I((int)x, (int)y), 0, Vector2I.Zero);
+			}
+		}
 	}
 
 	private void OnButtonPressed()
